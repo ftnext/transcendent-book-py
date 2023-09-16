fa78466f96bb93239698b8834580e0c5129e0821
ftnext <takuyafjp+develop@gmail.com>
2023-09-16 22:36:01 +0900

    [feat] 5の倍数の分岐に変更して通す (Green)

--- chapter6/test_first_bdd/src/core.py
+++ chapter6/test_first_bdd/src/core.py
@@ -2,6 +2,6 @@ class NumberConverter:
     def convert(self, n: int) -> str:
         if n % 3 == 0:
             return "Fizz"
-        if n == 5:
+        if n % 5 == 0:
             return "Buzz"
         return str(n)
