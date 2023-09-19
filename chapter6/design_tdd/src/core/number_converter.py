17cebd4d307b5460a383aa0a1b87dd896058de89
ftnext <takuyafjp+develop@gmail.com>
2023-09-19 22:19:51 +0900

    [feat] 仮実装でルールなしのケースを通す

--- chapter6/design_tdd/src/core/number_converter.py
+++ chapter6/design_tdd/src/core/number_converter.py
@@ -6,3 +6,6 @@ from src.core.replace_rule_interface import ReplaceRuleInterface
 class NumberConverter:
     def __init__(self, rules: Iterable[ReplaceRuleInterface]) -> None:
         self.rules = rules
+
+    def convert(self, n: int) -> str:
+        return ""
