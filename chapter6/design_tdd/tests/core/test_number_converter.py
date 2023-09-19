ef0ccf31b928d668f0492321c7412c13c4911bd3
ftnext <takuyafjp+develop@gmail.com>
2023-09-19 22:22:38 +0900

    [test] ルール1つありのテストケースを追加

--- chapter6/design_tdd/tests/core/test_number_converter.py
+++ chapter6/design_tdd/tests/core/test_number_converter.py
@@ -1,9 +1,18 @@
 from unittest import TestCase
+from unittest.mock import MagicMock
 
-from src.core import NumberConverter
+from src.core import NumberConverter, ReplaceRuleInterface
 
 
 class NumberConverterTestCase(TestCase):
     def test_convert_with_empty_rules(self):
         fizz_buzz = NumberConverter([])
         self.assertEqual("", fizz_buzz.convert(1))
+
+    def test_convert_with_single_rule(self):
+        rule = MagicMock(spec=ReplaceRuleInterface)
+        rule.replace.return_value = "Replaced"
+
+        fizz_buzz = NumberConverter([rule])
+        self.assertEqual("Replaced", fizz_buzz.convert(1))
+        rule.replace.assert_called_once_with(1)
